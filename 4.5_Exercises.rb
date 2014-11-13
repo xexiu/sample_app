dac9fa1e8b7bb7053e49420ccbe0a077c95170c2
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-13 22:25:40 +0100

    Created 4.5 Exercise

---
+++ 4.5_Exercises.rb
@@ -0,0 +1,15 @@
+["a", "b", "c", "d", "e"].to_a.split.shuffle.join('-')
+
+class String
+  def string_shuffle(s)
+    s.string('').shuffle.join
+  end
+end
+
+person1 = {}
+person2 = {}
+person3 = {}
+
+person1[:first]
+person1[:last]
+
