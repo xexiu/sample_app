06afdfbd397b008bf55792cf201256406a049e1c
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-14 16:14:27 +0100

    Generated User Controller

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,27 +1,4 @@
 require 'spec_helper'
 
 RSpec.describe "LayoutLinks", :type => :request do
-  describe "GET /layout_links" do
-
-    it "should have a HOME page" do
-      get '/'
-      expect(response.body).to match("<title>Home")
-    end
-
-    it "should have a Contact page at '/contact'" do
-      get '/contact'
-      expect(response.body).to match("<title>Contact")
-    end
-
-    it "should have an About page at '/about'" do
-      get '/about'
-      expect(response.body).to match("<title>About")
-    end
-
-    it "should have a Help page at '/help'" do
-      get '/help'
-      expect(response.body).to match("<title>Help")
-    end
-  end
-
 end
