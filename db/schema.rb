aabc55c449fde67c931bdf741228a8843753cc44
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-16 13:23:50 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115185826) do
+ActiveRecord::Schema.define(version: 20141116121808) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
