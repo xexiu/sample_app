b133b14dcd8a4595e0d2ae3a5155757dfa36766d
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-17 23:47:17 +0100

    User Login Success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user]) # Is the same as:
+    @user = User.new(user_params) # Is the same as:
     # @user = User.new(:name => "Foo Bar", :email => "foo@invalid",:password => "dude", :password_confirmation => "dude")
     if @user.save
     # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       @title = "Sign up"
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :salt, :encrypted_password)
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,26 +3,34 @@ require 'spec_helper'
 RSpec.describe UsersController, :type => :controller do
   render_views
 
-  describe "POST 'create'" do
-    describe "failure" do
-      before(:each) do
-        @attr = { :name => "", :email => "", :password => "",:password_confirmation => "" }
-        end
-    it "should not create a user" do
-      lambda do
-        post :create, :user => @attr
-      end.should_not change(User, :count)
-    end
-    it "should have the right title" do
-      post :create, :user => @attr
-      response.should have_selector("title", :content => "Sign up")
-    end
-    it "should render the 'new' page" do
-      post :create, :user => @attr
-      response.should render_template('new')
-    end
-  end
-end
+# =====================================================
+
+#   describe "POST 'create'" do
+#     describe "failure" do
+#       before(:each) do
+#         @attr = { :name => "", :email => "", :password => "",:password_confirmation => "" }
+#         end
+#     it "should not create a user" do
+#       lambda do
+#         post :create, :user => @attr
+#       end.should change(User, :count).by(1)
+#     end
+
+#     it "should redirect to the user show page" do
+#       post :create, :user => @attr
+#       response.should redirect_to(user_path(assigns(:user)))
+#     end
+
+#     it "should have the right title" do
+#       post :create, :user => @attr
+#       response.should have_selector("title", :content => "Sign up")
+#     end
+#     it "should render the 'new' page" do
+#       post :create, :user => @attr
+#       response.should render_template('new')
+#     end
+#   end
+# end
 
   # =============================================
 
