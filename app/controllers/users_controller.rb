68f240a857d7e14b00aa78d20dbac2e8f563425d
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-17 23:06:11 +0100

    Added Sign Up form + css style form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,15 @@ class UsersController < ApplicationController
     @title = "Sign Up"
   end
 
+  def create
+    @user = User.new(params[:user]) # Is the same as:
+    # @user = User.new(:name => "Foo Bar", :email => "foo@invalid",:password => "dude", :password_confirmation => "dude")
+    if @user.save
+    # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,27 @@ require 'spec_helper'
 RSpec.describe UsersController, :type => :controller do
   render_views
 
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",:password_confirmation => "" }
+        end
+    it "should not create a user" do
+      lambda do
+        post :create, :user => @attr
+      end.should_not change(User, :count)
+    end
+    it "should have the right title" do
+      post :create, :user => @attr
+      response.should have_selector("title", :content => "Sign up")
+    end
+    it "should render the 'new' page" do
+      post :create, :user => @attr
+      response.should render_template('new')
+    end
+  end
+end
+
   # =============================================
 
   # describe "GET 'show'" do
