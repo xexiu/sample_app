86b12cab5eb2230f676fd7627759b59ac7657508
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-24 17:24:53 +0100

    Model Microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124130415) do
+ActiveRecord::Schema.define(version: 20141124160943) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
