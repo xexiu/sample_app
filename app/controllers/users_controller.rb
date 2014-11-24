153fdf3648f725cf80c727906a8f63f9b55edcfc
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-24 12:58:24 +0100

    Redirect non-logged users + others

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
 
   def index
     @user = User.all
@@ -30,12 +31,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @title = "Edit user"
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated."
       redirect_to @user
@@ -49,6 +50,11 @@ class UsersController < ApplicationController
 
   def authenticate
     deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
   def user_params
